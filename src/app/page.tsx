5b9161353b620bedc8840bae758171b433cca9ba
Abhishek Badar <ab15.badar@gmail.com>
2023-10-14 17:01:38 +0530

    Fetched data and applied CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,34 @@ import { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 export default function Home() {
   const [inputVal, setInputVal] = useState("");
-  const {push} = useRouter();
-  const handleSubmit = (event:FormEvent) => {
+  const { push } = useRouter();
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     push(`/prediction/${inputVal}`);
-  }
+  };
   return (
-    <div>
-      <div>
-        <h1>Enter Your Name</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          Enter Your Name
+        </h1>
+      
+      <form onSubmit={handleSubmit} className="space-y-3">
         <input
           type="text"
-          placeholder="Enter your name"
+          placeholder="Enter your name.."
           value={inputVal}
-          className="text-black"
+          className="w-full p-2 border border-gray-300 rounded text-black"
           onChange={(e) => setInputVal(e.target.value)}
         />
-        <button type="submit">Predict Data</button>
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        >
+          Predict Data
+        </button>
       </form>
+    </div>
     </div>
   );
 }
